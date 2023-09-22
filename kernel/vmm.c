25436ac8783e7ae04cc776f94bb60c0eb45ce39c
deadfffool <deadfffool@gmail.com>
2023-09-22 09:11:48 +0800

    finish lab2_1

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -159,8 +159,13 @@ void *user_va_to_pa(pagetable_t page_dir, void *va) {
   // (va & (1<<PGSHIFT -1)) means computing the offset of "va" inside its page.
   // Also, it is possible that "va" is not mapped at all. in such case, we can find
   // invalid PTE, and should return NULL.
-  panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
 
+  // starting from the page directory
+  uint64 pa,val;
+  val = (uint64)va;
+  pa = lookup_pa(page_dir,val);
+  pa = pa + (val & ((1 << PGSHIFT) - 1));
+  return (void*)pa;
 }
 
 //
