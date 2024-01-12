9f5da7284ee102ebcce47d4b441f357d38fb8080
deadfffool <deadfffool@gmail.com>
2024-01-12 10:57:24 +0800

    finish lab

--- user/app_exec.c
+++ user/app_exec.c
@@ -4,9 +4,6 @@
 int main(int argc, char *argv[]) {
   printu("\n======== exec /bin/app_ls in app_exec ========\n");
   int ret = exec("/bin/app_ls");
-  if (ret == -1)
-    printu("exec failed!\n");
-
-  exit(0);
-  return 0;
+  printu("exec failed!\n");
+  exit(-1);
 }
