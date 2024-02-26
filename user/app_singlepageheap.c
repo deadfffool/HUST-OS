5a3f2dfa7bedd68986118090e4d2b2a91645789b
deadfffool <deadfffool@gmail.com>
2024-02-26 16:25:21 +0800

    update

--- user/app_singlepageheap.c
+++ user/app_singlepageheap.c
@@ -1,44 +1,70 @@
-/*
+// /*
+//  * Below is the given application for lab2_challenge2_singlepageheap.
+//  * This app performs malloc memory.
+//  */
+
+// #include "user_lib.h"
+// //#include "util/string.h"
+
+// typedef unsigned long long uint64;
+
+// char* strcpy(char* dest, const char* src) {
+//   char* d = dest;
+//   while ((*d++ = *src++))
+//     ;
+//   return dest;
+// }
+// int main(void) {
+  
+//   char str[20] = "hello, world!!!";
+//   char *m = (char *)better_malloc(100);
+//   // printu("user malloc 1\n");
+//   char *p = (char *)better_malloc(50);
+//   // printu("user malloc 2\n");
+//   if((uint64)p - (uint64)m > 512 ){
+//     printu("you need to manage the vm space precisely!");
+//     exit(-1);
+//   }
+//   better_free((void *)m);
+//   // printu("user free 1\n");
+//   strcpy(p,str);
+//   printu("%s\n",p);
+//   char *n = (char *)better_malloc(50);
+//   // printu("user malloc 3\n");
+//   if(m != n)
+//   {
+//     printu("your malloc is not complete.\n");
+//     exit(-1);
+//   }
+// //  else{
+// //    printu("0x%lx 0x%lx\n", m, n);
+// //  }
+//   exit(0);
+//   return 0;
+// }
+
+
+ /*
  * Below is the given application for lab2_challenge2_singlepageheap.
  * This app performs malloc memory.
  */
 
 #include "user_lib.h"
-//#include "util/string.h"
-
-typedef unsigned long long uint64;
-
-char* strcpy(char* dest, const char* src) {
-  char* d = dest;
-  while ((*d++ = *src++))
-    ;
-  return dest;
-}
+#include "util/types.h"
+#include "util/string.h"
 int main(void) {
   
-  char str[20] = "hello, world!!!";
+  char str[20] = "cross page";
   char *m = (char *)better_malloc(100);
-  // printu("user malloc 1\n");
-  char *p = (char *)better_malloc(50);
-  // printu("user malloc 2\n");
+  char *p = (char *)better_malloc(4096);  // cross page
   if((uint64)p - (uint64)m > 512 ){
     printu("you need to manage the vm space precisely!");
     exit(-1);
   }
   better_free((void *)m);
-  // printu("user free 1\n");
+
   strcpy(p,str);
   printu("%s\n",p);
-  char *n = (char *)better_malloc(50);
-  // printu("user malloc 3\n");
-  if(m != n)
-  {
-    printu("your malloc is not complete.\n");
-    exit(-1);
-  }
-//  else{
-//    printu("0x%lx 0x%lx\n", m, n);
-//  }
   exit(0);
   return 0;
 }
